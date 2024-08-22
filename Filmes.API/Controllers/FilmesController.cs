199978394bdf19f69b5552ce1beae5139a6cc1ca
marcoantonio <marcoantonio.parceiro@dnitservices.com>
2024-08-22 13:44:42 -0300

    Adicionado Método Get() retornando OK para verificar se o serviço está funcionando.

---
+++ Filmes.API/Controllers/FilmesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Filmes.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class FilmesController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok($"Serviço funcionando - Data e Hora Atual: {DateTime.Now.ToString("dd/MM/yyyy - HH:mm:ss")}");
+    }
+
+
+}
